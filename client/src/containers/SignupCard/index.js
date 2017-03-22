4639794440521c76c13472328e56763d579b8cbc
Kevin Norris <kevin.norris.90@gmail.com>
2017-03-23 12:36:27 +1300

    Implement basic signup form with validation

--- client/src/containers/SignupCard/index.js
+++ client/src/containers/SignupCard/index.js
@@ -1,16 +1,18 @@
 // npm packages
 import React from 'react';
-import {FormGroup, ControlLabel, FormControl, HelpBlock} from 'react-bootstrap';
+import {Button} from 'react-bootstrap';
 
 import Card from '../../components/Card';
+import * as val from '../../util/validation';
+import TextField from '../../components/TextField';
 
 // style
 import './signupCard.scss';
 
 const fieldValidations = [
-  ruleRunner('email', 'Email Address', required),
-  ruleRunner('password1', 'Password', required, minLength(6)),
-  ruleRunner('password2', 'Password Confirmation', mustMatch('password1', 'Password')),
+  val.ruleRunner('email', 'Email Address', val.required),
+  val.ruleRunner('password1', 'Password', val.required, val.minLength(6)),
+  val.ruleRunner('password2', 'Password Confirmation', val.mustMatch('password1', 'Password')),
 ];
 
 export default class SignupCard extends React.Component {
@@ -19,7 +21,18 @@ export default class SignupCard extends React.Component {
     password1: '',
     password2: '',
     showErrors: false,
-    validationErrors: {},
+    validationErrors: {
+      email: '',
+      password1: '',
+      password2: '',
+    },
+  }
+
+  errorFor(field) {
+    if (this.state.validationErrors[field]) {
+      return this.state.validationErrors[field];
+    }
+    return '';
   }
 
   handelInputChange(field) {
@@ -27,10 +40,22 @@ export default class SignupCard extends React.Component {
       const newState = {
         ...this.state,
         [field]: e.target.value,
-        validationErrors: run(newState, fieldValidations),
       };
+      newState.validationErrors = val.run(newState, fieldValidations);
+      console.log(newState);
       this.setState(newState);
     };
+  }
+
+  handleSubmitClicked = () => {
+    console.log('handelSubmitClicked');
+    console.log(this.state);
+    this.setState({
+      ...this.state,
+      showErrors: true,
+    });
+    // Check if errors exist
+    // ... continue submitting data to server
   }
 
   render() {
@@ -38,18 +63,32 @@ export default class SignupCard extends React.Component {
       <Card>
         <h1>Signup</h1>
         <form>
-          <FormGroup controlId="email" validationState={null}>
-            <ControlLabel>Email</ControlLabel>
-            <FormControl
-              name="email"
-              type="text"
-              placeholder="Your Email"
-              value={this.state.email}
-              onChange={this.handelInputChange('email')}
-            />
-            {this.state.showErrors ? <HelpBlock>{this.state.validationErrors.email}</HelpBlock> : ''}
-          </FormGroup>
+          <TextField
+            name="email"
+            placeHolder="Email address"
+            showError={this.state.showErrors}
+            text={this.state.email}
+            onFieldChange={this.handelInputChange('email')}
+            errorText={this.errorFor('email')}
+          />
+          <TextField
+            name="password1"
+            placeHolder="Password"
+            showError={this.state.showErrors}
+            text={this.state.password1}
+            onFieldChange={this.handelInputChange('password1')}
+            errorText={this.errorFor('password1')}
+          />
+          <TextField
+            name="password2"
+            placeHolder="Confirm password"
+            showError={this.state.showErrors}
+            text={this.state.password2}
+            onFieldChange={this.handelInputChange('password2')}
+            errorText={this.errorFor('password2')}
+          />
         </form>
+        <Button onClick={this.handleSubmitClicked}>Submit</Button>
       </Card>
     );
   }
