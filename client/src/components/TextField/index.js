d532d7ebe6c1495da061aec29406fda8e903c0db
Kevin Norris <kevin.norris.90@gmail.com>
2017-03-23 12:36:02 +1300

    Implement a form text field component
    Describes the display of validated for text fields

---
+++ client/src/components/TextField/index.js
@@ -0,0 +1,27 @@
+// npm packages
+import React from 'react';
+import {FormGroup, FormControl, HelpBlock} from 'react-bootstrap';
+
+const TextField = ({name, placeHolder, showError, text, onFieldChange, errorText}) => (
+  <FormGroup controlId={name} validationState={showError && errorText !== '' ? 'error' : null}>
+    <FormControl
+      name={name}
+      type="text"
+      placeholder={placeHolder}
+      value={text}
+      onChange={onFieldChange}
+    />
+    {showError && errorText !== '' ? <HelpBlock>{errorText}</HelpBlock> : ''}
+  </FormGroup>
+);
+
+TextField.propTypes = {
+  name: React.PropTypes.string.isRequired,
+  placeHolder: React.PropTypes.string.isRequired,
+  showError: React.PropTypes.bool.isRequired,
+  text: React.PropTypes.string.isRequired,
+  onFieldChange: React.PropTypes.func.isRequired,
+  errorText: React.PropTypes.string.isRequired,
+};
+
+export default TextField;
