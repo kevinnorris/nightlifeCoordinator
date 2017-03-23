69b5cabc516a0bea4ef7a277f1b6d4d4df8a5133
Kevin Norris <kevin.norris.90@gmail.com>
2017-03-23 14:32:52 +1300

    Addcommon passwords for use with validation

---
+++ client/src/util/constants.js
@@ -0,0 +1,32 @@
+// Very common passwords, and passwords based on app name
+// Should be extended to at least top 1k common passwords in non demo apps
+export const restrictedPasswords = [
+  'barsvp',
+  '123456',
+  'password',
+  '12345678',
+  'qwerty',
+  '12345',
+  '123456789',
+  'football',
+  '1234',
+  '1234567',
+  'baseball',
+  'welcome',
+  '1234567890',
+  'abc123',
+  '123abc',
+  '111111',
+  '1qaz2wsx',
+  'dragon',
+  'master',
+  'monkey',
+  'letmein',
+  'login',
+  'princess',
+  'qwertyuiop',
+  'passw0rd',
+  'starwars',
+  'default',
+];
+
