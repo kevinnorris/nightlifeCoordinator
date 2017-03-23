db385219798fb0a5b4a7722349504d2f56df091b
Kevin Norris <kevin.norris.90@gmail.com>
2017-03-23 14:35:46 +1300

    Style signupCard, update validation

--- client/src/containers/SignupCard/index.js
+++ client/src/containers/SignupCard/index.js
@@ -1,17 +1,19 @@
 // npm packages
 import React from 'react';
-import {Button} from 'react-bootstrap';
+import {Button, Row, Col} from 'react-bootstrap';
 
 import Card from '../../components/Card';
 import * as val from '../../util/validation';
 import TextField from '../../components/TextField';
+import {restrictedPasswords} from '../../util/constants';
 
 // style
 import './signupCard.scss';
 
 const fieldValidations = [
-  val.ruleRunner('email', 'Email Address', val.required),
-  val.ruleRunner('password1', 'Password', val.required, val.minLength(6)),
+  val.ruleRunner('email', 'Email Address', val.required, val.mustContain('@')),
+  val.ruleRunner('password1', 'Password', val.required, val.minLength(6),
+                val.cantContain(restrictedPasswords, ['email'])),
   val.ruleRunner('password2', 'Password Confirmation', val.mustMatch('password1', 'Password')),
 ];
 
@@ -48,8 +50,6 @@ export default class SignupCard extends React.Component {
   }
 
   handleSubmitClicked = () => {
-    console.log('handelSubmitClicked');
-    console.log(this.state);
     this.setState({
       ...this.state,
       showErrors: true,
@@ -60,36 +60,42 @@ export default class SignupCard extends React.Component {
 
   render() {
     return (
-      <Card>
-        <h1>Signup</h1>
-        <form>
-          <TextField
-            name="email"
-            placeHolder="Email address"
-            showError={this.state.showErrors}
-            text={this.state.email}
-            onFieldChange={this.handelInputChange('email')}
-            errorText={this.errorFor('email')}
-          />
-          <TextField
-            name="password1"
-            placeHolder="Password"
-            showError={this.state.showErrors}
-            text={this.state.password1}
-            onFieldChange={this.handelInputChange('password1')}
-            errorText={this.errorFor('password1')}
-          />
-          <TextField
-            name="password2"
-            placeHolder="Confirm password"
-            showError={this.state.showErrors}
-            text={this.state.password2}
-            onFieldChange={this.handelInputChange('password2')}
-            errorText={this.errorFor('password2')}
-          />
-        </form>
-        <Button onClick={this.handleSubmitClicked}>Submit</Button>
-      </Card>
+      <div className="SignupCard container">
+        <Row>
+          <Col xs={10} xsOffset={1} sm={8} smOffset={2} md={6} mdOffset={3} lg={4} lgOffset={4}>
+            <Card>
+              <h1 className="SignupCard-title">Signup</h1>
+              <form>
+                <TextField
+                  name="email"
+                  placeHolder="Email address"
+                  showError={this.state.showErrors}
+                  text={this.state.email}
+                  onFieldChange={this.handelInputChange('email')}
+                  errorText={this.errorFor('email')}
+                />
+                <TextField
+                  name="password1"
+                  placeHolder="Password"
+                  showError={this.state.showErrors}
+                  text={this.state.password1}
+                  onFieldChange={this.handelInputChange('password1')}
+                  errorText={this.errorFor('password1')}
+                />
+                <TextField
+                  name="password2"
+                  placeHolder="Confirm password"
+                  showError={this.state.showErrors}
+                  text={this.state.password2}
+                  onFieldChange={this.handelInputChange('password2')}
+                  errorText={this.errorFor('password2')}
+                />
+              </form>
+              <Button onClick={this.handleSubmitClicked}>Submit</Button>
+            </Card>
+          </Col>
+        </Row>
+      </div>
     );
   }
 }
