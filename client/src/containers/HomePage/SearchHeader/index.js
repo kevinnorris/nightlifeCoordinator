dfdbb4f45a17ace9393d495c540aecb163c35f22
Kevin Norris <kevin.norris.90@gmail.com>
2017-03-29 13:49:03 +1300

    Add placeholder to search input

--- client/src/containers/HomePage/SearchHeader/index.js
+++ client/src/containers/HomePage/SearchHeader/index.js
@@ -47,6 +47,7 @@ class Header extends React.Component {
                 <FormControl
                   type="text"
                   bsStyle="SearchHeader"
+                  placeholder="Search"
                   onChange={this.handelsearchChange}
                 />
                 <InputGroup.Button>
