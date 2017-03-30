3081bf5292f88290cd299b41fd52002e51410be7
Kevin N <kevin.norris.90@gmail.com>
2017-03-31 12:13:28 +1300

    Fix spelling

--- client/src/components/Intro/index.js
+++ client/src/components/Intro/index.js
@@ -10,7 +10,7 @@ export default () => (
   <Center large>
     <Card>
       <h1 className="text-center Intro-title">Search Your location!</h1>
-      <h2 className="text-center Intro-subTitle">Guage tonights bar scene and RSVP!</h2>
+      <h2 className="text-center Intro-subTitle">Gauge tonights bar scene and RSVP!</h2>
       <p className="text-center">Login/Signup to RSVP</p>
     </Card>
   </Center>
